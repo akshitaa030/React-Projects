7eb864b0835913983dce68f1e6dcd00f4e341aeb
akshitaa030 <96193220+akshitaa030@users.noreply.github.com>
2025-08-28 23:52:19 +0530

    Installing tailwind, Setting up theme and fonts

---
+++ Zentry Clone/src/App.jsx
@@ -0,0 +1,11 @@
+import "./index.css";
+
+function App() {
+  return (
+    <>
+      <p className="text-5xl text-violet-300 font-bold f">Welcome to Zentry</p>
+    </>
+  );
+}
+
+export default App;
